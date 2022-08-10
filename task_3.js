ba78da9852505a3e4850cf6951395a08cfdf5992
KonstantinChukavin <KainkosYa@yandex.ru>
2022-08-10 10:55:51 +0300

    complited task 3

---
+++ task_3.js
@@ -0,0 +1,12 @@
+/*
+Написать функцию, которая принимает число как аргумент и возвращает функцию, которая также принимает число как аргумент и возвращает сумму этих двух чисел. Выведите в консоль результат.
+*/
+
+function isFunctionOne(numOne) {
+    function isFunctionOTwo(numTwo) {
+        return numOne + numTwo;
+    }
+    return isFunctionOTwo(7);
+}
+let result = isFunctionOne(2);
+console.log(result);
